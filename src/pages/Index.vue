b7d82b18e36976989e797700280a06c1d4be2850
fokhub <65263096+fokhub@users.noreply.github.com>
2020-09-23 16:19:29 +0200

    Add Karlsruhe Webcam

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,9 +14,14 @@
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
-    
+    <p>
+    Izmir Webcam
+    </p>
     <link rel="stylesheet" type="text/css" href="https://images.webcamgalore.com/link.css" /><a href="https://www.webcamgalore.de/webcam/Tuerkei/Izmir/4420.html" class="wcglink wcglink-round wcglink-shadow" target="_blank"><img class="wcglink" src="https://images.webcamgalore.com/webcamimages/120x90/4420.jpg" alt="Webcam Izmir"/>Webcam Izmir</a>
-
+    <p>
+    Karlsruhe Webcam
+    </p>
+    <iframe width="500" height="540" src="https://www.panoramablick.com/embed/30349" frameborder="0"></iframe>
   </Layout>
 </template>
 
